7c6646f564fe0f852163ee1c91169d361e495c64
LucasMolter <lmmolter@gmail.com>
2023-05-18 14:22:11 -0400

    MNLIinstallation

--- Lucas/installation.py
+++ Lucas/installation.py
@@ -1,14 +1,15 @@
-
-import random
-import logging
-
-import tensorflow as tf
-
 from datasets import load_dataset
 
-dataset = load_dataset("wikitext", "wikitext-2-raw-v1")
-
-dataset.save_to_disk("/mnt/beegfs/bulk/stripe/lm865/WikiData/")
+mnli = load_dataset("multi_nli")
 
 #dataset.save_to_disk("/Users/lucasmolter/Documents/Cornell/Courses/Spring2023/CS5223 /togepi/Lucas/datatest")
+
+#mnli.save_to_disk("/Users/lucasmolter/Documents/Cornell/Courses/Spring2023/CS5223 /togepi/Lucas/MNLI")
+
+#dataset = load_dataset("wikitext", "wikitext-2-raw-v1")
+
+#dataset.save_to_disk("/mnt/beegfs/bulk/stripe/lm865/MNLI/")
+
+mnli.save_to_disk("/mnt/beegfs/bulk/stripe/lm865/MNLI/")
+
 
