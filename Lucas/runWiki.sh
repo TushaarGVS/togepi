b43c6e024f3d8261582eb7f546e570c9487625d9
LucasMolter <lmmolter@gmail.com>
2023-05-18 13:39:28 -0400

    test 8

--- Lucas/runWiki.sh
+++ Lucas/runWiki.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #SBATCH -N 1
 pip3 install -r requirements.txt
+echo "out of first requirements"
 pip3 install -r req2.txt
+echo "out of second requirements"
 python3 installation.py
+echo "installation ran"
