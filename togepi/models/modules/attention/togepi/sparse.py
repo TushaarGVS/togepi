Bug fix: softmax before causal mask and not after

togepi/models/modules/attention/togepi/sparse.py
@@ -7,6 +7,7 @@ class TogepiSparse(nn.Module):
         super().__init__()
 
         max_length = max_position_embeddings - 1  # one position reserved for pad position
+        self._training_max_length = max_length
 
         if sparse_init_dens is not None:
             num_nonzero = int(max_length * max_length * sparse_init_dens)
@@ -31,9 +32,11 @@ class TogepiSparse(nn.Module):
         max_length = pre_proj_emb.shape[1]
 
         sparse_data = self.sparse_mat
+        if self._training_max_length != max_length:
+            sparse_data = sparse_data[:max_length, :max_length]
         if self._causal:
             sparse_data = sparse_data.masked_fill(self.causal_sparse_mask[:max_length, :max_length] == 0, 0)
         pre_sparse_emb = self.pre_sparse_proj(pre_proj_emb)
         if padding_mask is not None:
             pre_sparse_emb.masked_fill_(padding_mask.unsqueeze(2) == 0, 0)
-        return torch.matmul(sparse_data, pre_sparse_emb)
+        return torch.matmul(sparse_data, pre_sparse_emb), sparse_data

togepi/models/modules/attention/togepi/toeplitz.py
@@ -20,7 +20,7 @@ class TogepiToeplitz(nn.Module):
         self.attn_actn = F.gelu if attn_actn == 'gelu' else F.relu
         self._softmax_psf_weights = softmax_psf_weights
         self.post_conv_proj = nn.Linear(in_features=embedding_dim, out_features=embedding_dim)
-        nn.init.xavier_normal_(self.toeplitz_psfs.data)
+        nn.init.trunc_normal_(self.toeplitz_psfs.data, mean=0.0, std=1.0, a=0.0, b=1.0)
 
         self._causal = causal_attn
         if causal_attn:
@@ -41,15 +41,19 @@ class TogepiToeplitz(nn.Module):
                                                                      self._per_head_dim).permute(0, 2, 1, 3)
 
         psfs_weights = self.toeplitz_psfs
-        if self._causal:
-            if self._training_max_length == max_length:
-                psfs_weights = psfs_weights.masked_fill(self.causal_psf_mask == 0, 0)
-            else:
-                # at inference time, the max_length changes per prompt
-                causal_psf_mask = torch.tensor([1] * max_length + [0] * (max_length - 1)).unsqueeze(0).unsqueeze(2)
-                psfs_weights = psfs_weights.masked_fill(causal_psf_mask == 0, 0)
+        if self._training_max_length == max_length:
+            causal_psf_mask = self.causal_psf_mask
+        else:
+            # at inference time, the max_length changes per prompt
+            curr_prev_toks_psfs_weights = psfs_weights[:, : max_length, :]
+            next_toks_psfs_weights = \
+                psfs_weights[:, self._training_max_length: self._training_max_length + max_length - 1, :]
+            psfs_weights = torch.hstack((curr_prev_toks_psfs_weights, next_toks_psfs_weights))
+            causal_psf_mask = torch.tensor([1] * max_length + [0] * (max_length - 1)).unsqueeze(0).unsqueeze(2)
         if self._softmax_psf_weights:
             psfs_weights = F.softmax(psfs_weights, dim=1)
+        if self._causal:
+            psfs_weights = psfs_weights.masked_fill(causal_psf_mask == 0, 0)
 
         # fft: convolution in time domain is point-wise multiplication in frequency domain
         # note: cuFFT doesn't support half-precision for dimensions that aren't powers of two, which is the case
@@ -63,4 +67,4 @@ class TogepiToeplitz(nn.Module):
         conv_output = self.attn_actn(conv_output).permute(0, 2, 1, 3).reshape(batch_size, max_length, -1)
         conv_emb = self.post_conv_proj(conv_output)
 
-        return conv_emb
+        return conv_emb, psfs_weights

togepi/models/modules/attention/togepi/togepi_mha.py
@@ -44,9 +44,11 @@ class TogepiMultiHeadAttention(nn.Module):
             pre_proj_emb.masked_fill_(padding_mask.unsqueeze(2) == 0, 0)
 
         # conv_emb: (batch_size, max_length, embedding_dim)
-        conv_emb = self.toeplitz(pre_proj_emb)
+        # psfs_weights: (num_heads, 2 * max_length - 1, per_head_dim)
+        conv_emb, psfs_weights = self.toeplitz(pre_proj_emb)
         # sparse_emb: (batch_size, max_length, embedding_dim)
-        sparse_emb = self.sparse(pre_proj_emb, padding_mask=padding_mask)
+        # sparse_mat: (mqx_length, max_length)
+        sparse_emb, sparse_mat = self.sparse(pre_proj_emb, padding_mask=padding_mask)
         togepi_emb = self.dropout(conv_emb + sparse_emb) * 1 / self._keep_prob
 
-        return self.layer_norm(togepi_emb + embeddings), None  # None added to keep consistent with bert output format
+        return self.layer_norm(togepi_emb + embeddings), (psfs_weights, sparse_mat)

togepi/models/modules/encoder.py
@@ -61,13 +61,13 @@ class Encoder(nn.Module):
 
     def forward(self, input_ids, token_type_ids=None, padding_mask=None):
         embeddings = self.emb(input_ids=input_ids, token_type_ids=token_type_ids, padding_mask=padding_mask)
-        emb_all_layers, attn_filters_all_layers = [], []
+        emb_all_layers, attn_filters_or_psfs_all_layers = [], []
         for enc_layer in self.enc_layers:
             embeddings, attn_filters = enc_layer(embeddings, padding_mask=padding_mask)
             emb_all_layers.append(embeddings)
             if attn_filters is not None:
-                attn_filters_all_layers.append(attn_filters)
+                attn_filters_or_psfs_all_layers.append(attn_filters)
 
-        if len(attn_filters_all_layers) == 0:
-            attn_filters_all_layers = None
-        return emb_all_layers, attn_filters_all_layers
+        if len(attn_filters_or_psfs_all_layers) == 0:
+            attn_filters_or_psfs_all_layers = None
+        return emb_all_layers, attn_filters_or_psfs_all_layers

togepi/models/transformer.py
@@ -54,11 +54,16 @@ class Transformer(nn.Module):
     def from_pretrained(self, model_save_path):
         self.load_state_dict(torch.load(model_save_path))
 
-    def forward(self, input_ids, token_type_ids=None, padding_mask=None):
-        emb_all_layers, attn_filters_all_layers = self.enc(input_ids=input_ids, token_type_ids=token_type_ids,
-                                                           padding_mask=padding_mask)
+    def forward(self, input_ids, token_type_ids=None, padding_mask=None, return_attn_filters_or_psfs=False):
+        emb_all_layers, attn_filters_or_psfs_all_layers = self.enc(input_ids=input_ids, token_type_ids=token_type_ids,
+                                                                   padding_mask=padding_mask)
         last_emb = emb_all_layers[-1]
-        del emb_all_layers, attn_filters_all_layers  # clear out cuda memory
+
+        # clear out cuda memory
+        if not return_attn_filters_or_psfs:
+            del attn_filters_or_psfs_all_layers
+        del emb_all_layers
+
         if self._use_lm_head:
             lm_output = self.lm_head(last_emb)
         else:
@@ -66,7 +71,7 @@ class Transformer(nn.Module):
             # last_emb: (batch_size, max_length, embedding_dim)
             # tok_emb_weight: (vocab_size, embedding_dim)
             lm_output = torch.matmul(last_emb, self.enc.emb.tok_emb.weight.data.transpose(0, 1))
-        return lm_output
+        return lm_output if not return_attn_filters_or_psfs else (lm_output, attn_filters_or_psfs_all_layers)
 
     @torch.no_grad()
     def generate(self, input_ids, max_new_tokens, temperature=1.0, num_samples=1, top_k=None):